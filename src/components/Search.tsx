b3617f6fee9262bf9ff32140214365ff51f1b542
tchamidaryl <tchamidaryl@gmail.com>
2026-01-30 19:00:06 +0100

    Search functionality implemented

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,103 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { Input } from "./ui/input";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
+import Thumbnail from "./Thumbnail";
+import FormattedDateTime from "./FormattedDateTime";
 
 const Search = () => {
-    return <div>Search</div>;
+    const router = useRouter();
+    const path = usePathname();
+    const [query, setQuery] = useState("");
+    const searchParams = useSearchParams();
+    const searchQuery = searchParams.get("query") || "";
+    const [results, setResults] = useState<Models.Document[]>([]);
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        const fetchFiles = async () => {
+            if (!query) {
+                setResults([]);
+                setOpen(false);
+                return router.push(path.replace(searchParams.toString(), ""));
+            }
+
+            const files = await getFiles({ searchText: query });
+            setResults(files.documents);
+            setOpen(true);
+        };
+        fetchFiles();
+    }, [query]);
+
+    useEffect(() => {
+        if (!searchQuery) {
+            setQuery("");
+        }
+    }, []);
+
+    const handleClickItem = (file: Models.Document) => {
+        setOpen(false);
+        setResults([]);
+        router.push(
+            `/${file.type === "video" || file.type === "audio" ? "media" : file.type + "s"}?query=${query}`,
+        );
+    };
+
+    return (
+        <div className="search">
+            <div className="search-input-wrapper">
+                <Image
+                    src="/assets/icons/search.svg"
+                    alt="Search"
+                    width={24}
+                    height={24}
+                />
+                <Input
+                    value={query}
+                    placeholder="Search ..."
+                    onChange={(e) => setQuery(e.target.value)}
+                    className="search-input"
+                />
+
+                {open && (
+                    <ul className="search-result">
+                        {results.length > 0 ? (
+                            results.map((file) => (
+                                <li
+                                    className="flex items-center justify-between"
+                                    key={file.$id}
+                                    onClick={() => handleClickItem(file)}
+                                >
+                                    <div className="flex cursor-pointer items-center gap-4">
+                                        <Thumbnail
+                                            type={file.type}
+                                            extension={file.extension}
+                                            url={file.url}
+                                            className="size-9 min-w-9"
+                                        />
+                                        <p className="subtitle-2 line-clamp-1 text-light-100">
+                                            {file.name}
+                                        </p>
+                                    </div>
+
+                                    <FormattedDateTime
+                                        date={file.$createdAt}
+                                        className="caption line-clamp-1 text-light-100"
+                                    />
+                                </li>
+                            ))
+                        ) : (
+                            <p className="empty-result">No files found</p>
+                        )}
+                    </ul>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Search;

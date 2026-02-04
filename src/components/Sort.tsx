018f261447d5e27396ce449a47b7ec83e63339d8
tchamidaryl <tchamidaryl@gmail.com>
2026-02-04 19:51:16 +0100

    Sorting implemented

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
+import React from "react";
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "../components/ui/select";
+import { usePathname, useRouter } from "next/navigation";
+import { sortTypes } from "@/constants";
 
 const Sort = () => {
-  return (
-    <div>Sort</div>
-  )
-}
+    const router = useRouter();
+    const path = usePathname();
 
-export default Sort
+    const handleSort = (value: string) => {
+        router.push(`${path}?sort=${value}`);
+    };
+
+    return (
+        <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+            <SelectTrigger className="sort-select">
+                <SelectValue placeholder={sortTypes[0].value} />
+            </SelectTrigger>
+            <SelectContent className="sort-select-content">
+                <SelectGroup>
+                    {sortTypes.map((sort) => {
+                        const { value, label } = sort;
+                        return (
+                            <SelectItem key={label} value={value}>
+                                {label}
+                            </SelectItem>
+                        );
+                    })}
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    );
+};
+
+export default Sort;
